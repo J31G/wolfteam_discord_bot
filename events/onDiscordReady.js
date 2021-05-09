bcab4956bb7b4627fc447c77bde258835f8f7cbb
Jamie <jamie@lambertstock.com>
2021-05-09 21:04:13 +0100

    Added in auto delete error messages after 3 seconds

--- events/onDiscordReady.js
+++ events/onDiscordReady.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 module.exports.onDiscordReady = (client) => {
   const wokClient = new WOKCommands(client, {
     commandsDir: 'commands',
+    del: 3,
     testServers: ['825352046605238352', '322328346799243264'],
     dbOptions: {
       keepAlive: true,
