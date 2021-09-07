59accd0f1404a55f2ca7b457c207d5c1a6d4d7d7
Jamie <jamie@lambertstock.com>
2021-09-07 23:54:20 +0000

    Added in clear comment as MEE6 was down

---
+++ commands/clearchat.js
@@ -0,0 +1,36 @@
+module.exports = {
+  slash: false,
+  testOnly: true,
+  description: 'Clear given amount of messages the channel',
+  aliases: ['clear'],
+  category: 'General',
+  callback: async ({ message, args }) => {
+    if (!message.member.permissions.has('MANAGE_MESSAGES')) return message.reply('You do not have permission to do that').then((msg) => setTimeout(() => msg.delete(), 3000));
+
+    // Get out mentioned user and the amount of messages to delete
+    const user = await message.mentions.users.first();
+    const amount = parseInt(args[0], 10) ? parseInt(args[0], 10) : parseInt(args[1], 10);
+
+    // Check if amount entered / user tagged
+    if (!amount) return message.reply('Must specify an amount to delete!').then((msg) => setTimeout(() => msg.delete(), 3000));
+    if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!').then((msg) => setTimeout(() => msg.delete(), 3000));
+
+    // Array to collect message id's to delete
+    const messageIDs = [];
+
+    // If a user was tagged, get their messages
+    if (user) {
+      await message.channel.messages
+        .fetch({ limit: 100 })
+        .then((msg) => msg.filter((m) => m.author.id === user.id))
+        .then((msg) => Array.from(msg).slice(0, amount))
+        .then((msg) => msg.forEach((element) => messageIDs.push(element[0])));
+    }
+
+    // Now delete the messages
+    return message.channel.bulkDelete(!user ? amount : messageIDs, true)
+      .then((messages) => message.channel.send(`${messages.size} Messages deleted`))
+      .then((msg) => setTimeout(() => msg.delete(), 3000))
+      .catch(console.error);
+  },
+};
